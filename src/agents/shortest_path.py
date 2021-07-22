245f19fdcfef44667a1162c2c00512e13017fb17
Adel Sohbi <adel.sohbi@gmail.com>
2021-07-22 13:28:51 +0900

    implement shortest path agent

---
+++ src/agents/shortest_path.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Shorthest Path Agent for Quantum Internet Network Routing.
+"""
+
+import networkx as nx
+from networkx.classes import Graph
+from agents.agent import Agent
+
+class ShortestPathAgent(Agent):
+    """
+    Agent that selects the shortest path between the sender and reciever on the current state of the virtual network.
+    """
+
+    def __init__(self,
+                 physical_network: Graph,
+                 virtual_network: Graph) -> None:
+        """
+        Args:
+            physical_network: classical network
+            virtual_network: initial quantum network
+        """
+        
+        # Get parameters
+        self._physical_network = physical_network
+        self._virtual_network = virtual_network
+
+        # Initialize number of epochs
+        self._N = 0
+
+    def policy(self, state, sender, reciever) -> int:
+        """ Compute the shortest path and the action is the node right after the source.
+        Returns:
+            The action.
+        """
+        
+        path = nx.shortest_path(self._virtual_network, source=sender, target=reciever, weight=None, method='dijkstra')
+        action = path[1]
+        return action
+
+    def _run(self, state, sender, reciever, reward):
+
+        action = self.policy(state, sender, reciever)
+
+        return action
